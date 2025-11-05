c484df10246c1e5134b18987dd93ad425ddd31e0
shyqwy <shayni.del@gmail.com>
2025-11-06 00:24:00 +0800

    added kiosk functions

--- routes/kiosk.js
+++ routes/kiosk.js
@@ -12,28 +12,4 @@ router.get("/", (req, res, next) => {
   });
 });
 
-// ✅ Example: COR process page
-router.get('/cor', (req, res, next) => {
-  res.render('kiosk/cor', {}, (err, htmlContent) => {
-    if (err) return next(err);
-    res.render('layout_kiosk', { pageTitle: 'COR Process', content: htmlContent });
-  });
-});
-
-// ✅ Example: Checklist page
-router.get('/checklist', (req, res, next) => {
-  res.render('kiosk/checklist', {}, (err, htmlContent) => {
-    if (err) return next(err);
-    res.render('layout_kiosk', { pageTitle: 'Checklist', content: htmlContent });
-  });
-});
-
-// ✅ Example: Queue confirmation page
-router.get('/queue', (req, res, next) => {
-  res.render('kiosk/queue', {}, (err, htmlContent) => {
-    if (err) return next(err);
-    res.render('layout_kiosk', { pageTitle: 'Queue Number', content: htmlContent });
-  });
-});
-
 module.exports = router;
